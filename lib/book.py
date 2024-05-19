cccf8eb9dc2ff5565cd7ab5faaf4db6542a479cb
achoclate <nyabokeann99@gmail.com>
2024-05-18 21:18:15 -0700

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None  # Initial placeholder to bypass setter in __init__
+        self.page_count = page_count  # Trigger setter for validation
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
     
         
